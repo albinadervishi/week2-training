6a86021ae9fab53ec8c9acc5768946a8c5872fdd
HugoLAMOUREUX <hugo.lamoureux18@gmail.com>
2025-04-30 16:16:33 +0200

    up modal

--- app/src/components/modal.jsx
+++ app/src/components/modal.jsx
@@ -1,10 +1,10 @@
-import React, { Fragment, useRef } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import { HiX } from 'react-icons/hi'
+import React, { Fragment, useRef } from "react"
+import { Dialog, Transition } from "@headlessui/react"
+import { HiX } from "react-icons/hi"
 
-import { cn } from '@/utils'
+import { cn } from "@/utils"
 
-export default function Modal({ isOpen, children, onClose, className = 'w-[calc(100%_-_60px)]' }) {
+export default function Modal({ isOpen, children, onClose, className = "w-[calc(100%_-_60px)]" }) {
   const cancelButtonRef = useRef()
 
   if (!isOpen) return <Fragment />
@@ -12,42 +12,42 @@ export default function Modal({ isOpen, children, onClose, className = 'w-[calc(
   return (
     <Transition show={isOpen} as={Fragment}>
       <Dialog
-        as='div'
-        className='fixed inset-0 z-50 overflow-y-auto bg-[#D9D9D9] bg-opacity-30 backdrop-blur-sm'
+        as="div"
+        className="fixed inset-0 z-50 overflow-y-auto bg-[#D9D9D9] bg-opacity-30 backdrop-blur-sm"
         initialFocus={cancelButtonRef}
         open={isOpen}
         onClose={onClose ? onClose : () => {}}
       >
-        <div className='min-h-screen px-4'>
+        <div className="min-h-screen px-4">
           <Transition.Child
             as={Fragment}
-            enter='ease-out duration-300'
-            enterFrom='opacity-0'
-            enterTo='opacity-100'
-            leave='ease-in duration-200'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <Dialog.Overlay className='fixed inset-0' />
+            <Dialog.Overlay className="fixed inset-0" />
           </Transition.Child>
           <Transition.Child
             as={Fragment}
-            enter='ease-out duration-300'
-            enterFrom='opacity-0 scale-95'
-            enterTo='opacity-100 scale-100'
-            leave='ease-in duration-200'
-            leaveFrom='opacity-100 scale-100'
-            leaveTo='opacity-0 scale-95'
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
           >
             <div
               className={cn(
-                'w-full z-10 bg-white rounded-3xl shadow-md absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 max-h-screen overflow-y-auto',
+                "w-full z-10 bg-white rounded-3xl shadow-md absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 max-h-screen overflow-y-auto",
                 className
               )}
             >
               {onClose && (
-                <div className='absolute cursor-pointer top-5 right-5'>
-                  <HiX className='text-xl text-gray-500 transition-colors hover:text-red-500' onClick={onClose} />
+                <div className="absolute cursor-pointer top-5 right-5">
+                  <HiX className="text-xl text-gray-500 transition-colors hover:text-red-500" onClick={onClose} />
                 </div>
               )}
               {children}
